ce93a157df1f4d1e9b54b897081437c5d04ecb36
Daniel Tkach <danieltkach@gmail.com>
2025-09-16 20:36:35 -0300

    account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+  [HttpPost("register")] // api/account/register
+  public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+  {
+    var hmac = new HMACSHA512();
+    var user = new AppUser
+    {
+      DisplayName = displayName,
+      Email = email,
+      PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+      PasswordSalt = hmac.Key
+    };
+
+    context.Users.Add(user);
+    await context.SaveChangesAsync();
+
+    return user;
+  }
+}
