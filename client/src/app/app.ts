602a1f676812edde0c423284f2c723f48b4f4e49
Daniel Tkach <danieltkach@gmail.com>
2025-09-16 11:13:51 -0300

    HTTP request

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,16 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private http = inject(HttpClient);
   protected readonly title = signal('client');
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+    });
+  }
+
 }
