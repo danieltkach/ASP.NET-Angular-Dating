d029c16fb689da8a4643d210779a4fa3e06f12c4
Daniel Tkach <danieltkach@gmail.com>
2025-09-16 13:48:14 -0300

    signal, mapping

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -10,10 +10,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 export class App implements OnInit {
   private http = inject(HttpClient);
   protected readonly title = signal('client');
+  protected members = signal<any>([]);
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => console.log(response),
+      next: response => this.members.set(response),
       error: error => console.log(error),
       complete: () => console.log('Completed the http request')
     });
