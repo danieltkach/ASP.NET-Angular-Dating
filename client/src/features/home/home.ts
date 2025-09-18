2079583f35308b047020df2e89c16787627c9ccf
Daniel Tkach <danieltkach@gmail.com>
2025-09-18 11:37:18 -0300

    home

---
+++ client/src/features/home/home.ts
@@ -0,0 +1,18 @@
+import { Component, inject, Input, signal } from '@angular/core';
+import { Register } from "../account/register/register";
+import { AccountService } from '../../core/services/account-service';
+
+@Component({
+  selector: 'app-home',
+  imports: [Register],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+  protected registerMode = signal(false);
+  protected accountService = inject(AccountService);
+
+  showRegister(value: boolean) {
+    this.registerMode.set(value);
+  }
+}
