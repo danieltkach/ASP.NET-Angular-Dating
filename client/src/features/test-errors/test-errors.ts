e69a2bd35689250d443f580cbcbfb3964c7de639
Daniel Tkach <danieltkach@gmail.com>
2025-09-19 09:45:51 -0300

    error interceptor

--- client/src/features/test-errors/test-errors.ts
+++ client/src/features/test-errors/test-errors.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-test-errors',
@@ -8,12 +9,13 @@ import { Component, inject } from '@angular/core';
 })
 export class TestErrors {
   private http = inject(HttpClient);
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
+  validationErrors = signal<string[]>([]);
 
   get404Error() {
     this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
       next: response => console.log(response),
-      error: error => console.error(error)
+      error: error => console.log(error)
     });
   }
 
@@ -27,21 +29,24 @@ export class TestErrors {
   get500Error() {
     this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
       next: response => console.log(response),
-      error: error => console.error(error)
+      error: error => console.log(error)
     });
   }
 
   get401Error() {
     this.http.get(this.baseUrl + 'buggy/auth').subscribe({
       next: response => console.log(response),
-      error: error => console.error(error)
+      error: error => console.log(error)
     });
   }
 
   get400ValidationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.error(error)
+      error: error => {
+        console.log(error);
+        this.validationErrors.set(error);
+      }
     });
   }
 }
