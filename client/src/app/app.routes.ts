a5c03ae9bb7fdd14e7231965a22717cc36a5a371
Daniel Tkach <danieltkach@gmail.com>
2025-09-19 14:34:26 -0300

    error interceptors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { MemberList } from '../features/members/member-list/member-list';
 import { Messages } from '../features/messages/messages';
 import { authGuard } from '../core/guards/auth-guard';
 import { TestErrors } from '../features/test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
+import { ServerError } from '../shared/errors/server-error/server-error';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -21,5 +23,6 @@ export const routes: Routes = [
     ]
   },
   { path: 'errors', component: TestErrors },
-  { path: '**', component: Home }
+  { path: 'server-errors', component: ServerError },
+  { path: '**', component: NotFound }
 ];
