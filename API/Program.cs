933287f84cfc59ca5c5335333f678b7de7e8fb51
Daniel Tkach <danieltkach@gmail.com>
2025-09-18 21:30:17 -0300

    demo error controller

--- API/Program.cs
+++ API/Program.cs
@@ -33,7 +33,6 @@ var app = builder.Build();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
