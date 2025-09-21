70abbada1af57b83744370560302a2d4b47dd018
Daniel Tkach <danieltkach@gmail.com>
2025-09-21 16:51:23 -0300

    profile content

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-profile',
   imports: [],
   templateUrl: './member-profile.html',
 })
-export class MemberProfile {
+export class MemberProfile implements OnInit {
+  private route = inject(ActivatedRoute);
+  protected member = signal<Member | undefined>(undefined);
 
+  ngOnInit(): void {
+    this.route.parent?.data.subscribe(data => {
+      this.member.set(data['member']);
+    });
+  }
 }
