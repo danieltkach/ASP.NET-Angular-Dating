985471104149715f74add963d6a3a6f55079e5cc
Daniel Tkach <danieltkach@gmail.com>
2025-09-21 18:30:40 -0300

    photos component

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MemberService } from '../../../core/services/member-service';
+import { Observable } from 'rxjs';
+import { Photo } from '../../../types/member';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-photos.html',
 })
 export class MemberPhotos {
+  private memberService = inject(MemberService);
+  private route = inject(ActivatedRoute);
+  protected photos$?: Observable<Photo[]>;
 
+  constructor() {
+    const memberId = this.route.parent?.snapshot.paramMap.get('id');
+    if (memberId) {
+      this.photos$ = this.memberService.getMemberPhotos(memberId);
+    }
+  }
 }
